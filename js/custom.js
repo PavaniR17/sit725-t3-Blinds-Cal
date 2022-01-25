efdbd937e361953679932cff2a5deb0e5191a011
EMPIRE\samka <skamande@deakin.edu.au>
2022-01-25 14:46:08 +1100

    Created the layout for the homepage

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+M.AutoInit();
+ 
+ document.addEventListener('DOMContentLoaded', function() {
+    //var elems = document.querySelectorAll('.carousel');
+    var instance = M.Carousel.init({
+        numVisible: 2,
+        indicators: true
+    });
+  });
