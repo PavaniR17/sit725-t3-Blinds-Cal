78fff6220a52566952c97e36b7f6264f1ed75e30
EMPIRE\samka <skamande@deakin.edu.au>
2022-01-31 06:20:41 +1100

    created the login functionality

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,35 @@ M.AutoInit();
         indicators: true
     });
   });
+
+
+//login form validation
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
+
+
+  if (username === "admin" && password === "blinds") {
+
+    redirectUser();
+
+  } else {
+
+      loginErrorMsg.style.display = "block";
+
+  }
+
+});
+
+function redirectUser(){
+
+  window.location.assign("http://127.0.0.1:5500/index.html");
+
+}
